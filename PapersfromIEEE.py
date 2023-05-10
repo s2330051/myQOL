29c0b38683e64938799c80b0eeaa3d2d343d88be
s2330051 <s2330051@edu.cc.uec.ac.jp>
2023-05-11 06:23:18 +0900

    first commit

---
+++ PapersfromIEEE.py
@@ -0,0 +1,34 @@
+import os
+import requests
+
+# IEEE Xplore APIのエンドポイントとパラメータを設定する
+url = 'https://ieeexploreapi.ieee.org/api/v1/search/articles'
+api_key = 'y46pfqdx2r5p3re878yzjwrr'
+headers = {
+    'X-API-Key': api_key,
+    'Content-Type': 'application/json'
+}
+params = {
+    'querytext': 'Federated Learning',  # 検索キーワードを指定する
+    'max_records': 30,  # 取得する論文数を指定する
+    'sort_order': 'desc',  # 降順でソートする
+    'sort_field': 'publication_year'  # 発行年でソートする
+}
+
+# IEEE Xplore APIから論文情報を取得する
+response = requests.post(url, headers=headers, json=params)
+data = response.json()['articles']
+
+# 論文をダウンロードする
+folder = 'C:/Users/PC_User/Desktop/paper'
+for article in data:
+    title = article['title']
+    abstract = article['abstract']
+    if 'Federated Learning' in title or 'Federated Learning' in abstract:
+        pdf_url = article['pdf_url']
+        response = requests.get(pdf_url)
+        filename = f"{title}.pdf"
+        filepath = os.path.join(folder, filename)
+        with open(filepath, 'wb') as f:
+            f.write(response.content)
+        print(f"{filename} saved to {folder}")
---
+++ aggregate_papers.py
@@ -0,0 +1,38 @@
+import os
+import requests
+import feedparser
+import re
+
+# arXiv APIのエンドポイントとパラメータを設定する
+url = 'http://export.arxiv.org/api/query'
+params = {
+    'search_query': 'Federated Learning OR federated learning',  # 検索キーワードを指定する
+    'max_results': 50,  # 取得する論文数を指定する
+    'sortBy': 'lastUpdatedDate',  # 更新日時でソートする
+    'sortOrder': 'descending'  # 降順でソートする
+}
+
+# arXiv APIから論文情報を取得する
+response = requests.get(url, params=params)
+feed = feedparser.parse(response.text)
+
+# 論文をダウンロードする
+folder = 'C:/Users/PC_User/Desktop/paper'
+for entry in feed.entries:
+
+
+    pdf_url = entry.links[1].href  # PDFのURLを取得する
+    response = requests.get(pdf_url)
+
+    # ダウンロードしたPDFをローカルフォルダに保存する
+    title = re.sub(r'[\\/*?:"<>|]+', '', entry.title)
+
+
+    if 'Federated Learning' in entry.title:
+        filename = f"{title}.pdf"
+        filename = filename.replace('\n', '')  # 改行文字を削除する
+        filepath = os.path.join(folder, filename)
+        with open(filepath, 'wb') as f:
+            f.write(response.content)
+
+        print(f"{filename} saved to {folder}")
